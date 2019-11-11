bb87d7d4cb26418e21f3bdd80901bc8345c8ac8d
pkyrylchenko <57421952+pkyrylchenko@users.noreply.github.com>
2019-11-10 23:18:49 -0500

    Update _const.cs

--- ReportDeployment.Logic/_const.cs
+++ ReportDeployment.Logic/_const.cs
@@ -11,5 +11,24 @@ namespace Report_Deployment_Wizard
         #region script const
         public const string C_CreateDataSource = "dsCreate.ps1"; 
         #endregion
+        #region shared ds path
+        public const string C_SharedDSPath = "/";
+        #endregion
+        #region ERP DS name
+        public const string C_ERPSharedDSName = "LYNQmomERP";
+        #endregion
+        #region LYNQ DS name
+        public const string C_LYNQSharedDSName = "LYNQmom";
+
+        #endregion
+        #region RS.EXE path
+        public const string C_RS_EXE_PATH = @"C:\Program Files\Microsoft SQL Server Reporting Services\Shared Tools";
+        #endregion
+        #region deploy reports script path
+        public const string C_DeployReportsScriptPath = @"C:\Tools\RDL\rss\deploy.rss";
+        #endregion
+        #region registry ssrs location
+        public const string C_RegistrySSRSLocation = @"SOFTWARE\Microsoft\Microsoft SQL Server\SSRS";
+        #endregion
     }
 }
